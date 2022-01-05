fa3494c9bed698d1cd773578bd7a65d4e8f9b240
fanlia <3093932086@qq.com>
2022-01-05 09:21:03 +0800

    refine

--- install-elasticsearch-plugins.sh
+++ install-elasticsearch-plugins.sh
@@ -6,6 +6,6 @@ do
     echo $version elasticsearch${version%%.*}
     test -f elasticsearch-analysis-ik-$version.zip || wget https://github.com/medcl/elasticsearch-analysis-ik/releases/download/v$version/elasticsearch-analysis-ik-$version.zip
     test -f elasticsearch-analysis-stconvert-$version.zip || wget https://github.com/medcl/elasticsearch-analysis-stconvert/releases/download/v$version/elasticsearch-analysis-stconvert-$version.zip
-    docker-compose exec -e version=${version} elasticsearch${version%%.*} sh /tmp/elasticsearch-plugins/install-plugins.sh
-    docker-compose restart elasticsearch${version%%.*}
+    sudo docker-compose exec -e version=${version} elasticsearch${version%%.*} sh /tmp/elasticsearch-plugins/install-plugins.sh
+    sudo docker-compose restart elasticsearch${version%%.*}
 done
