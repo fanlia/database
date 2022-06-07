1cd534ba80f796cee0704f4f05ccb421b8860d50
fanlia <you@example.com>
2022-06-07 10:03:55 +0800

    add analysis-hao

--- elasticsearch-plugins/install-plugins.sh
+++ elasticsearch-plugins/install-plugins.sh
@@ -1,3 +1,6 @@
 target=`pwd`
 elasticsearch-plugin install file:///tmp/elasticsearch-plugins/elasticsearch-analysis-ik-$version.zip --batch
 elasticsearch-plugin install file:///tmp/elasticsearch-plugins/elasticsearch-analysis-stconvert-$version.zip --batch
+if [ $version == "7.17.2" ]; then
+    elasticsearch-plugin install file:///tmp/elasticsearch-plugins/analysis-hao-v$version.zip --batch
+fi
--- install-elasticsearch-plugins.sh
+++ install-elasticsearch-plugins.sh
@@ -6,6 +6,9 @@ do
     echo $version elasticsearch${version%%.*}
     test -f elasticsearch-analysis-ik-$version.zip || wget https://github.com/medcl/elasticsearch-analysis-ik/releases/download/v$version/elasticsearch-analysis-ik-$version.zip
     test -f elasticsearch-analysis-stconvert-$version.zip || wget https://github.com/medcl/elasticsearch-analysis-stconvert/releases/download/v$version/elasticsearch-analysis-stconvert-$version.zip
+    if [ $version == "7.17.2" ]; then
+        test -f analysis-hao-v$version.zip || wget https://github.com/tenlee2012/elasticsearch-analysis-hao/releases/download/v$version/analysis-hao-v$version.zip
+    fi
     sudo docker-compose exec -e version=${version} elasticsearch${version%%.*} sh /tmp/elasticsearch-plugins/install-plugins.sh
     sudo docker-compose restart elasticsearch${version%%.*}
 done
